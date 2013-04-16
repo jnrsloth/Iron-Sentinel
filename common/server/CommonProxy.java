9709c37f8e2523e3b58e97c1f26518340a92051a
Supernaturali <jnrsloth@yahoo.com.au>
2013-04-16 14:58:22 +1000

    updating Initial commit

---
+++ common/server/CommonProxy.java
@@ -0,0 +1,12 @@
+package server;
+
+public class CommonProxy {
+        public static String ITEMS_PNG = "/Iron Sentinel/IS_resource/Items.png";
+        public static String BLOCK_PNG = "/Iron Sentinel/IS_resource/Blocks.png";
+
+        // Client stuff
+        public void registerRenderers() {
+            // Nothing here as the server doesn't render graphics!
+        }
+
+}
